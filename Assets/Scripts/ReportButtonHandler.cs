18040741904ccc0d8c75f9e7f01632b57dd5a3b7
Jere Koivuranta <jere.koivuranta@gmail.com>
2026-03-17 13:59:55 +0200

    asetussivun pohjaa tehty. ei mitaan kayttokelpoista

---
+++ Assets/Scripts/ReportButtonHandler.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReportButtonHandler : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HandleReportClick()
+    {
+        // oikeasti lisäisi käyttäjän jättämän raportin esim. tietokantaan
+        Debug.Log("report button toimii");
+    }
+}
