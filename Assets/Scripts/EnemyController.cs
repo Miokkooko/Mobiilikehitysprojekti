84da38fbb31051aa799a6e7b85cb47ec65994033
Jere Koivuranta <jere.koivuranta@gmail.com>
2026-03-19 16:26:05 +0200

    viholliset seuraa pelaajaa

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,20 +6,19 @@ public class EnemyController : MonoBehaviour
     
     public Rigidbody2D rigidbody;
     public float movementSpeed = 1f;
-    
+
+    private GameObject playerToFollow;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerToFollow = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //rigidbody.linearVelocity = UnityEngine.Vector2.down;
-
-        transform.position = UnityEngine.Vector3.MoveTowards(transform.position, new UnityEngine.Vector3(0,0,0), movementSpeed * Time.deltaTime);
+        transform.position = UnityEngine.Vector3.MoveTowards(transform.position, new UnityEngine.Vector3(playerToFollow.transform.position.x, playerToFollow.transform.position.y, 0), movementSpeed * Time.deltaTime);
     }
 
     public void DestroyEnemy()
